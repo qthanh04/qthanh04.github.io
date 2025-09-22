93640a1ed9ea994258926ddb21a05c579ff1b795
qthanh04 <129851317+qthanh04@users.noreply.github.com>
2025-09-22 04:14:53 +0000

    Deploying to gh-pages from @ qthanh04/qthanh04.github.io@a5a3f81b45010b7a7e37ecc8f392402e4191a7df 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "qthanh04/qthanh.github.io",
+    "data-repo": "qthanh04/qthanh04.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
